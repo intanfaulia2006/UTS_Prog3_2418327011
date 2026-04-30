28abe7ab95bcec4492bfb9ecb4e63f5e81f8f948
intanfaulia2006 <intanfaulia2006@gmail.com>
2026-04-30 12:42:45 +0700

    Add files via upload

---
+++ Pertemuan6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan6;
+
+/**
+ *
+ * @author INTAN
+ */
+public class Pertemuan6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
